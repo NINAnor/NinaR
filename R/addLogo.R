8d73cb5b0f58f67352dbe7f6df9e3036e2e190f0
Jens Åström <jenast@gmail.com>
2016-04-14 22:03:28 +0200

    Forgot to stage in commit, see earlier commit message

--- R/addLogo.R
+++ R/addLogo.R
@@ -1,27 +1,40 @@
 #' addLogo
 #'
-#' # Adds a NINA logo to plot
+#' Adds a NINA logo to plot
 #'
-#' Colors conform to the 'NINA Grafisk handbok'
+#' Logo is based on Postscript version of logo, and is scalable
+#' (vectorized). The function uses the package grImport. See
+#' examples for usage.
 #'
-#' @param Fill in
-#' @return Fill in
+#' @param x x-position in percentage values (0-1) of
+#' npc (Normalized parent coordinates)
+#' @param y y-position in percentage values (0-1) of
+#' npc (Normalized parent coordinates)
+#' @param size size specified as width in percentage of original size (default 0.2).
+#' Height is left unspecified to scale with width, retaining
+#' original aspect ratio.
+#'
+#' @return Adds scalable logo on current device (opens new device if no current device exists)
 #' @author Jens Astrom
 #' @examples
-#' plot((1:10)^2, 1:10, col=NinaPalette(), cex=4, pch=16, las=1, type="n")
+#' ## Add small logo to right bottom corner
+#' plot((1:10)^2, 1:10, col=NinaPalette(), cex=4, pch=16, las=1)
 #' addLogo()
+#'
+#' #Add large logo to background of plot
+#' plot((1:10)^2, 1:10, col=NinaPalette(), cex=4, pch=16, las=1, type="n")
+#' addLogo(x = 0.5, y = 0.5, size = 1)
+#' grid.rect(gp = gpar(fill = rgb(1, 1, 1, .6)))
 #' points((1:10)^2, 1:10, col=NinaPalette(), cex=4, pch=16)
 #' @export
 
-addLogo <- function(x = 0.85, y = 0.1) {
-xpos=x
-ypos=y
+addLogo <- function(x = 0.85, y = 0.1, size = 0.2) {
 require(grImport)
 require(grid) ##Necessary?
 
 #PostScriptTrace(system.file("img/logo.ps", package = "NinaR"))
 logo <- readPicture(system.file("img/logo.ps.xml", package = "NinaR"))
 pushViewport(plotViewport())
-grid.picture(logo, width=0.2, x = unit(xpos, "npc"), y = unit(ypos, "npc"))
+grid.picture(logo, width = size, x = unit(x, "npc"), y = unit(y, "npc"))
 }
 
