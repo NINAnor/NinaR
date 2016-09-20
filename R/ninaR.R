115873cff92e2073f76efb6868aaaa61bcb2278b
unknown <jens.astrom@NINAPC208.nina.no>
2016-09-20 09:16:54 +0200

    Added NinaR help page.

---
+++ R/ninaR.R
@@ -0,0 +1,14 @@
+#' NinaR: document templates and functions for NINA
+#'
+#' NinaR provides helper functions for using NINA color schemes
+#' and logo in R graphics, and provides document templates
+#' for presentations and reports using RMarkdown syntax.
+#'
+#'
+#' To learn more about NinaR, start with the vignettes:
+#' \code{browseVignettes(package = "NinaR")}
+#'
+#' @docType package
+#' @name NinaR
+#' @import rmarkdown
+NULL
