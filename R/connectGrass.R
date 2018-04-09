9026319ba4ed7e267d99d91834f3719b2eb0c4ab
ninsbl <stefan.blumentrath@gmx.de>
2018-04-09 12:45:51 +0200

    remove version from grass connection

--- R/connectGrass.R
+++ R/connectGrass.R
@@ -27,7 +27,7 @@ connectGrass <- function(){
       location <- "ETRS_33N"
       mapset <- paste("u_", user, sep = "")
       wd <- paste(gisDbase, location, mapset, sep = "/")
-      try(system(paste("grass72 -text -c -e", wd)))
+      try(system(paste("grass -text -c -e", wd)))
       initGRASS(gisBase = "/usr/lib/grass72/", location = location,
                 mapset = mapset, gisDbase = gisDbase, override = TRUE)
     } else
