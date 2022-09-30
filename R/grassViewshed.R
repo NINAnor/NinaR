9bd04aac3b78af726ffecf9a06987f74d0798499
Jens Åström <jens.astrom@nina.no>
2022-09-30 14:49:02 +0200

    updated read_VECT function name, error handling and example.

--- R/grassViewshed.R
+++ R/grassViewshed.R
@@ -12,12 +12,23 @@
 #' @param target_elevation numerical. The user can change this using option target_elevation to determine
 #' if objects of a given height would be visible.
 #'
-#' @return NULL
-#' @author  Jens Astrom
+#' @return A SpatVector object (terra)
+#' @author  Jens Åström
 #' @examples
 #' \dontrun{
-#' hm <- grassViewshed(xycoords = data.frame("x" = 310400, "y" = 6991600), dist=1000)
-#' plot(hm)
+#'test_view <- grassViewshed(xycoords = data.frame("x" = 310400,
+#'                                                 "y" = 6991600),
+#'                           dist=1000)
+#'
+#'
+#' terra::plot(test_view)
+#'
+#' test_view_sf <- sf::st_as_sf(test_view)
+#' ggplot(test_view_sf) +
+#' geom_sf(aes(fill = value)) +
+#' scale_fill_nina(discrete = FALSE)
+#'
+#'
 #' }
 #' @export
 
@@ -28,7 +39,7 @@ grassViewshed <- function(xycoords, return = TRUE, dist = 100, observer_elevatio
 
   #Check that is is run on NINSRV16
   #Check that is is run on NINSRV16
-  if(!checkMachine()){
+  if(!NinaR:::checkMachine()){
     stop("Must be run on NINA servers!")
   }
 
@@ -38,7 +49,7 @@ grassViewshed <- function(xycoords, return = TRUE, dist = 100, observer_elevatio
   op <- options(warn=2)
   tt <- try(gmeta())
   if(is(tt,"try-error")){
-    NinaR::connectGrass()
+    NinaR::grassConnect()
     }
   options(op)
 
@@ -62,11 +73,15 @@ grassViewshed <- function(xycoords, return = TRUE, dist = 100, observer_elevatio
                                             max_distance = dist),
                                             flags = c("b", "c","overwrite"))
 
-  execGRASS("r.to.vect", parameters = list(input = "viewshed_raster", output = "viewshed_vector", type = "area"),
-            flags = c("overwrite"))
+  execGRASS("r.to.vect",
+            parameters = list(input = "viewshed_raster",
+                              output = "viewshed_vector",
+                              type = "area"),
+            flags = c("overwrite"),
+            ignore.stderr = TRUE)
 
-  if(return==T){
-  out <- readVECT("viewshed_vector")
+  if(return){
+  out <- rgrass7::read_VECT("viewshed_vector")
   return(out)
   }
 
